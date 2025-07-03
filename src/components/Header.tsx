74e4970e4136dcc68901ee8fa10fadbb2a03e5a5
Georgios Tselikas <georgios.p.tselikas@gmail.com>
2025-07-03 19:40:58 +0300

    feat: add responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,17 @@
 import CFLogo from "./CFLogo.tsx";
+import {Link} from "react-router"
 
 const Header =() => {
     return (
         <>
             <header className="bg-[#782024] fixed w-full">
                 <div className="container mx-auto px-4 flex items-center justify-between">
-                    <CFLogo/>
-                    <a className="text-white hover:underline-offset-4" href="/">Home</a>
+                  <CFLogo/>
+                  {/*<a className="text-white hover:underline-offset-4" href="/">Home</a>*/}
+                  <nav className="flex gap-4">
+                    <Link to="/" className="text-white hover:underline hover:underline-offset-4">Home</Link>
+                    <Link to="examples/name-changer" className="text-white hover:underline hover:underline-offset-4">Name Changer</Link>
+                  </nav>
                 </div>
             </header>
         </>
